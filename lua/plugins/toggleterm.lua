8abfcdeae88703ea37038f82150fe4a9ef058fac
renatosanz <renato.sanchez3103@gmail.com>
2024-12-11 09:31:17 -0600

    keymap & term changes

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require("term").setup({
 				shell = vim.o.shell,
-				width = 0.5,
+				width = 0.8,
 				height = 0.5,
 				anchor = "NW",
 				position = "center",
