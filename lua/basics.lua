8f60539ed0ed24de9049b55ff169f02c63f33581
renatosanz <renato.sanchez3103@gmail.com>
2026-07-11 21:08:55 -0600

    [Keymaps] Update keymaps

--- lua/basics.lua
+++ lua/basics.lua
@@ -40,7 +40,7 @@ map("i", "jk", "<esc>")
 map("n", "<leader>ww", ":w!<CR>")
 map("n", "<leader>wq", ":wq!<CR>")
 map("n", "<leader>l", ":Lazy<CR>")
-map("n", "<C-S>", ":%s/")
+map("n", "<C-S>", "/")
 map("n", "sp", ":sp<CR>")
 map("n", "tj", ":tabprev<CR>")
 map("n", "tk", ":tabnext<CR>")
@@ -50,12 +50,40 @@ map("n", "vs", ":vs<CR>")
 map("n", "<leader>j", ":cnext<CR>", { silent = true })
 map("n", "<leader>k", ":cprevious<CR>", { silent = true })
 map("n", "<leader>o", ":tabonly<cr>:only<CR>", { silent = true })
+
 map("n", "<c-h>", "<c-w>h")
 map("n", "<c-l>", "<c-w>l")
 map("n", "<c-j>", "<c-w>j")
 map("n", "<c-k>", "<c-w>k")
+
+map("n", "<leader>sv", "<C-w>v") -- Split vertically
+map("n", "<leader>sh", "<C-w>s")
+
+map("n", "<C-Up>", ":resize +2<CR>")
+map("n", "<C-Down>", ":resize -2<CR>")
+map("n", "<C-Left>", ":vertical resize +2<CR>")
+map("n", "<C-Right>", ":vertical resize -2<CR>")
+
 map("n", "fd", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true, desc = "Toggle terminal" })
 map("n", "<C-w>", ":bd<CR>", { desc = "Close current buffer" })
+map({ "n", "x", "o" }, "H", "^")
+map({ "n", "x", "o" }, "L", "$")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+map("n", "Y", "y$")
+
+-- paste without losing buffer
+map("v", "p", '"_dP')
+
+-- Yank entire buffer into default register
+map("n", "yag", ":%y<CR>", { noremap = true, silent = true })
+
+-- execute commands with enter
+map("n", "<CR>", ":", { desc = "Enter command mode" })
+
+-- search and replace
+map("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { silent = false })
 
 local opts = { buffer = 0 }
 map("t", "<esc>", [[<C-\><C-n>]], opts)
